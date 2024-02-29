19ebcc1f4c0affce48be6e14ff935bdd482e9dc7
Nat Niemeir <nniemeir@protonmail.com>
2024-02-28 19:08:00 -0600

    Began revamping Cryptography I

--- C++/2. Cryptography I - Caesar Ciphers/shift.cpp
+++ C++/2. Cryptography I - Caesar Ciphers/shift.cpp
@@ -2,9 +2,82 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <optional>
+
+const std::string FILE_NAME = "target.txt";
+const int ALPHABET_LENGTH = 26;
+const int ASSUMED_MOST_FREQUENT = 4;
+
+std::string encrypt(const std::string& target, int key) {
+	std::string cipherText;
+	for (char i : target)
+        {
+            int test = isalpha(i);
+            if (test != 0)
+            {
+                if (isupper(i))
+                {
+                    i = (i - 'A' + key) % ALPHABET_LENGTH + 'A';
+                }
+                else
+                {
+                    i = (i - 'a' + key) % ALPHABET_LENGTH + 'a';
+                }
+            }
+            cipherText += i;
+        }
+        return cipherText;
+}
+
+std::string decrypt(const std::string& target, int key) {
+	std::string clearText;
+	for (char i : target)
+        {
+            int test = isalpha(i);
+            if (test)
+            {
+                if (isupper(i))
+                {
+                    i = (i - 'A' - key + ALPHABET_LENGTH) % ALPHABET_LENGTH + 'A';
+                }
+                else
+                {
+                    i = (i - 'a' - key + ALPHABET_LENGTH) % ALPHABET_LENGTH + 'a';
+                }
+            }
+	    clearText += i;
+        }
+	return clearText;
+}
+
+std::optional<int> findKey(const std::string& target) {
+const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+            std::vector<int> occurrences;
+            for (char i : alphabet)
+            {
+                std::string::difference_type charFreq = std::count(target.begin(), target.end(), i);
+                occurrences.push_back(charFreq);
+            }
+            auto it = std::max_element(occurrences.begin(), occurrences.end());
+            if (it != occurrences.end())
+            {
+                int index = std::distance(occurrences.begin(), it);
+                std::cout << "Most Common Letter: " << alphabet[index] << "\n";
+                if (index != ASSUMED_MOST_FREQUENT)
+                {
+                    int diff = abs(index - ASSUMED_MOST_FREQUENT);
+                    if (index < ASSUMED_MOST_FREQUENT)
+                    {
+                        diff = ALPHABET_LENGTH - diff;
+                    }
+		    return diff;
+                }
+	    }
+return std::nullopt;
+}
+
 int main()
 {
-    system("clear");
     std::ifstream target_File("target.txt");
     target_File.seekg(0, std::ios::end);
     size_t size = target_File.tellg();
@@ -19,23 +92,8 @@ int main()
     {
         std::cout << "Enter a ROT key: ";
         std::cin >> key;
-        for (char i : target)
-        {
-            int test = isalpha(i);
-            if (test)
-            {
-                if (isupper(i))
-                {
-                    i = (i - 'A' + key) % 26 + 'A';
-                }
-                else
-                {
-                    i = (i - 'a' + key) % 26 + 'a';
-                }
-            }
-            std::cout << i;
-        }
-        std::cout << std::endl;
+	std::string cipherText = encrypt(target, key);
+	std::cout << cipherText << "\n~~~\n\n"; 
     }
     else if (choice == 2)
     {
@@ -43,63 +101,22 @@ int main()
         int index;
         int choice2;
         std::cin >> choice2;
-        system("clear");
         if (choice2 == 1)
         {
-            std::cout << "\nEnter the key" << std::endl;
+            std::cout << "\nEnter the key: ";
             std::cin >> key;
         }
         else if (choice2 == 2)
         {
-            const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
-            std::vector<int> occurrences;
-            for (char i : alphabet)
-            {
-                std::string::difference_type charFreq = std::count(target.begin(), target.end(), i);
-                occurrences.push_back(charFreq);
-            }
-            auto it = std::max_element(occurrences.begin(), occurrences.end());
-            if (it != occurrences.end())
-            {
-                int index = std::distance(occurrences.begin(), it);
-                std::cout << "Most Common Letter: " << alphabet[index] << std::endl;
-                if (index != 4)
-                {
-                    int diff = abs(index - 4);
-                    if (index < 4)
-                    {
-                        diff = 26 - diff;
-                    }
-                    std::cout << "Most Likely Cipher: ROT" << diff << std::endl;
-                }
-                else
-                {
-                    std::cout << "The text does not appear to have been shifted" << std::endl;
-                }
-            }
+	std::optional<int> guessedKey = findKey(target);
         }
-        std::cout << "Output:\n\n~~~" << std::endl;
-        for (char i : target)
-        {
-            int test = isalpha(i);
-            if (test)
-            {
-                if (isupper(i))
-                {
-                    i = (i - 'A' - key + 26) % 26 + 'A';
-                }
-                else
-                {
-                    i = (i - 'a' - key + 26) % 26 + 'a';
-                }
-            }
-                std::cout << i;
-        }
-        std::cout << "\n~~~\n" << std::endl;
+        std::cout << "Output:\n\n~~~" << "\n";
+	std::string clearText = decrypt(target, key);
+        std::cout << clearText << "\n~~~\n\n";
     }
     else
     {
-        std::cout << "Invalid Option, Please Try Again" << std::endl;
+        std::cout << "Invalid Option, Please Try Again\n";
     }
     return 0;
 }
