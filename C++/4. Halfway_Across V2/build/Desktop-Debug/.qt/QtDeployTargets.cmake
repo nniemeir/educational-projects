05c50d41e6690a8764967842735d9680af0d4863
Jacob Niemeir <nniemeir@protonmail.com>
2024-09-04 06:53:22 -0500

    Removed unneeded files

--- C++/4. Halfway_Across V2/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Halfway_Across_FILE /home/noodle/MEGA/Educational_Projects/C++/4. Halfway_Across V2/build/Desktop-Debug/Halfway_Across)
-set(__QT_DEPLOY_TARGET_Halfway_Across_TYPE EXECUTABLE)
