8b467f7a4a89ae1e32f9541fd49df134cc02612b
Jacob Niemeir <nniemeir@protonmail.com>
2024-12-20 20:14:44 -0600

    Modularity improvments

--- C/2. minServer/src/response.c
+++ C/2. minServer/src/response.c
@@ -19,21 +19,55 @@ char *get_metadata(int response_code) {
   case 413:
     return HTTP_413;
     break;
+  default:
+    return NULL;
   }
-  return NULL;
+}
+
+FILE *open_file(const char *file_request) {
+  FILE *file = fopen(file_request, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Unable to open file %s", file_request);
+  }
+  return file;
+}
+
+// Read file in chunks to avoid buffer overflow
+size_t read_file_chunks(FILE *file, char *response, size_t file_size,
+                        size_t metadata_length, size_t available_space) {
+  size_t bytes_read = 0;
+  while (bytes_read < file_size) {
+    size_t chunk_size = file_size - bytes_read < available_space
+                            ? file_size - bytes_read
+                            : available_space;
+    size_t read =
+        fread(response + metadata_length + bytes_read, 1, chunk_size, file);
+    if (read == 0) {
+      if (ferror(file)) {
+        fprintf(stderr, "Failed to read file.\n");
+        fclose(file);
+        break;
+      }
+      break;
+    }
+    bytes_read += read;
+  }
+  return bytes_read;
 }
 
 char *add_file_to_response(char *method, char *file_request, char *response,
                            int *response_code) {
-  FILE *file = fopen(file_request, "r");
-  if (file == NULL) {
-    fprintf(stderr, "Unable to open file %s", file_request);
+  FILE *file = open_file(file_request);
+  if (!file) {
+    fclose(file);
     return NULL;
   }
 
   char *metadata = get_metadata(*response_code);
   if (!metadata) {
+    fclose(file);
     fprintf(stderr, "Failed to match error code to string.\n");
+    return NULL;
   }
 
   size_t file_size = get_file_size(file_request);
@@ -48,51 +82,26 @@ char *add_file_to_response(char *method, char *file_request, char *response,
 
   memcpy(response, metadata, metadata_length);
   if (strcmp(method, "GET") == 0) {
-    size_t bytes_read = 0;
-    while (bytes_read < file_size) {
-      // Read file in chunks to avoid buffer overflow
-      size_t chunk_size = file_size - bytes_read < available_space
-                              ? file_size - bytes_read
-                              : available_space;
-      size_t read =
-          fread(response + metadata_length + bytes_read, 1, chunk_size, file);
-      if (read == 0) {
-        if (ferror(file)) {
-          fprintf(stderr, "Failed to read file.\n");
-          fclose(file);
-          return NULL;
-        }
-        break;
-      }
-      bytes_read += read;
-    }
+    read_file_chunks(file, response, file_size, metadata_length,
+                     available_space);
   }
   fclose(file);
   return response;
 }
 
-char *generate_response(char *buffer) {
-  // Allocate memory for response string
+char *allocate_response() {
   char *response = malloc(BUFFER_SIZE);
-  int response_code;
   if (!response) {
-    fprintf(stderr, "Failed to allocate memory for response.\n");
     return NULL;
   }
   memset(response, 0, BUFFER_SIZE);
+  return response;
+}
 
-  char *method = get_method(buffer);
-  if (!method) {
-    response_code = 405;
-    response = add_file_to_response(method, "website/405.html", response,
-                                    &response_code);
-    return response;
-  }
-
+char *isolate_file_request(char *buffer) {
   // Isolate file_request
   char *buffer_copy = strdup(buffer);
   if (!buffer_copy) {
-    free(response);
     fprintf(stderr, "Failed to make copy of buffer.\n");
     return NULL;
   }
@@ -102,6 +111,27 @@ char *generate_response(char *buffer) {
   if (space_position) {
     *space_position = '\0';
   }
+  return file_request;
+}
+
+char *generate_response(char *buffer) {
+  char *response;
+  response = allocate_response();
+  if (!response) {
+    fprintf(stderr, "Failed to allocate memory for response.\n");
+    return NULL;
+  }
+
+  int response_code;
+  char *method = get_method(buffer);
+  if (!method) {
+    response_code = 405;
+    response = add_file_to_response(method, "website/405.html", response,
+                                    &response_code);
+    return response;
+  }
+
+  char *file_request = isolate_file_request(buffer);
 
   response_code = 200;
   int valid_path = validate_path(file_request);
@@ -118,7 +148,6 @@ char *generate_response(char *buffer) {
   response =
       add_file_to_response(method, file_request, response, &response_code);
   if (!response) {
-    free(buffer_copy);
     fprintf(stderr, "Failed to add file to response.\n");
     return NULL;
   }
@@ -128,12 +157,9 @@ char *generate_response(char *buffer) {
 
   if (!log_request(buffer, response_code, response_size)) {
     fprintf(stderr, "Failed to log request.");
-    free(buffer_copy);
     free(response);
     return NULL;
   }
-
-  free(buffer_copy);
 
   return response;
 }
