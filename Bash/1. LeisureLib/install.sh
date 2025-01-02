74b5f0bf288769a2bcfa0ea80d32c446b1f5dcb8
Jacob Niemeir <nniemeir@protonmail.com>
2025-01-01 22:08:35 -0600

    Added image previews and system-wide installation

---
+++ Bash/1. LeisureLib/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if ! command -v gzip &>/dev/null; then
+    echo "Missing dependency: gzip"
+    exit 1
+fi
+
+if ! sudo cp -f leisure /usr/bin/; then
+    echo "Failed to move script to /usr/bin"
+    exit 1
+fi
+
+CONFIG_DIR="$HOME/.config/leisure/"
+
+if [ ! -d "$CONFIG_DIR" ]; then
+    if ! mkdir -p "$CONFIG_DIR"; then
+        echo "Failed to make configuration directory."
+        exit 1
+    fi
+    if ! cp -f config/previews/ "$CONFIG_DIR/"; then
+        echo "Failed to copy previews directory."
+        exit 1
+    fi
+fi
+
+if ! cp config/preferences.conf config/games.csv "$CONFIG_DIR"; then
+    echo "Failed to copy configuration files to configuration directory."
+    exit 1
+fi
+
+if ! gzip <man/leisure.1 > man/leisure.1.gz; then
+    echo "Failed to compress manpage."
+fi
+
+if ! sudo cp -f man/leisure.1.gz /usr/share/man/man1/; then
+    echo "Failed to install manual page."
+    exit 1
+fi
+
+if ! rm -rf man/leisure.1.gz; then
+    echo "Failed to remove temporary files."
+    exit 1
+fi
+
+echo "Install successful."
---
+++ Bash/1. LeisureLib/uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! sudo rm -rf /usr/share/man/man1/leisure.1; then
+    echo ""
+    exit 1
+fi
+
+if ! sudo rm -rf /usr/bin/leisure; then
+    echo "Failed to remove script"
+    exit 1
+fi
+
+echo "Uninstall successful."
